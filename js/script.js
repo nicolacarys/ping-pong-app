97a60e28b3e55641f4a3a4af3fcc286f706cb94a
Nicola Richardson <nicolarichardson@Nicolas-MacBook-Pro.local>
2017-09-13 19:31:57 +0100

    updated storage so data is retained across browser windows and persists on reload

--- js/script.js
+++ js/script.js
@@ -7,19 +7,26 @@ $($ => {
 	// storing the number of players inputted on the first page
 	let inputEntry = $(".js__entry-numbers");
 	let buttonEntry = $(".js__button-entry-numbers");
-	let players = "";
 
-	buttonEntry.on("click", () => players = inputEntry.val());
+	// allowing data to be stored across HTML windows
+	let myStorage = window.localStorage;
 
+	buttonEntry.on("click", e => {
+		// e.preventDefault();
+
+		players = inputEntry.val();
+		myStorage.setItem("players", inputEntry.val());
+		
+	});
 // ****************************
 // 	PLAYER INFORMATION PAGE
 // ****************************
 
 	// console-logging the number inputted from the first page
 	let buttonPlayer = $(".js__button-players");
+	let container = $(".container");
 
-	buttonPlayer.on("click", () => console.log(players));
-
+	buttonPlayer.on("click", () => console.log(myStorage.getItem("players")));
 
 });
 
