bbe94b17e63e81e8708feda9ee0e4c443a88580d
Nicola Richardson <nicolarichardson@Nicolas-MacBook-Pro.local>
2017-09-14 17:39:00 +0100

    able to store and output player details on button click

--- js/script.js
+++ js/script.js
@@ -12,23 +12,29 @@ $($ => {
 
 	let inputsContainer = $(".js__player-details-container");
 	let buttonPlayer = $(".js__button-players");
-
+	
+	let forenames = [];
+	let surnames = [];
 	let playerDetails = [];
 
+	let trackerPage = $(".js__tracker-page");
+
+
 // ****************************
-// 	EVENTS
+// 	NUMBER OF PLAYERS SECTION
 // ****************************
 
 	buttonEntry.on("click", () => {
+
 		// log the number of players in variable
 		players = inputEntry.val();
 
-		// output a corresponding number of inputs
 		let i = 0;
 
+		// output a corresponding number of inputs
 		while (i < players) {
 			let fieldContainer = $("<div />");
-			fieldContainer.addClass("inputs-container js__inputs-container-" + i);
+			fieldContainer.addClass("inputs-container js__inputs-container").attr("data-id", i);
 
 			let labelFor = $("<label />");
 			labelFor.attr("id", "player-forename").text("First Name");
@@ -45,8 +51,55 @@ $($ => {
 			inputsContainer.append(fieldContainer);
 			fieldContainer.append(labelFor, forename, labelSur, surname);
 
-			i ++;
+			i++;
 		}
+	});
+
+// ****************************
+// 	PLAYER DETAILS SECTION
+// ****************************
+
+	buttonPlayer.on("click", () => {
+
+		inputsContainer.find(".js__inputs-container .js__forename-input").each(function () {
+			forenames.push($(this).val());
+		});
+
+		inputsContainer.find(".js__inputs-container .js__surname-input").each(function () {
+			surnames.push($(this).val());
+		});
+
+		// storing the details as objects in an array
+		for (let i = 0; i < forenames.length; i++){
+			let player = {
+				id: i,
+				forename: forenames[i],
+				surname: surnames[i],
+			};
+
+			playerDetails.push(player);
+			// console.log(playerDetails);
+
+		};
+
+// ****************************
+// 	TRACKER SECTION  - - - - - - but currently operating on previous section click
+// ****************************
+
+		// concatenating the names for display purposes
+		let fullName = playerDetails.map(({ forename, surname}) => forename + " " + surname.charAt(0));
+
+		let playerList = $("<ul />");
+		trackerPage.append(playerList);
+
+		// outputting an individual <li> for each player name
+		fullName.map(player => {
+			let listItem = $("<li />").text(player);
+			playerList.append(listItem);
+		})
+
+		// console.log(fullName);
+
 	});
 
 });
