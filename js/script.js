0cf79ad936dd457483376b5b97213730b7b7b7a0
Nicola Richardson <nicolarichardson@Nicolas-MacBook-Pro.local>
2017-09-14 12:10:56 +0100

    changed app to single page, outputs inputs based on player number

--- js/script.js
+++ js/script.js
@@ -1,32 +1,53 @@
 $($ => {
 
 // ****************************
-// 	NUMBER OF ENTRANTS PAGE
+// 	VARIABLES
 // ****************************
 
-	// storing the number of players inputted on the first page
+	let container = $(".js__wrapper");
+
 	let inputEntry = $(".js__entry-numbers");
 	let buttonEntry = $(".js__button-entry-numbers");
+	let players = "";
 
-	// allowing data to be stored across HTML windows
-	let myStorage = window.localStorage;
-
-	buttonEntry.on("click", e => {
-		// e.preventDefault();
-
-		players = inputEntry.val();
-		myStorage.setItem("players", inputEntry.val());
-		
-	});
-// ****************************
-// 	PLAYER INFORMATION PAGE
-// ****************************
-
-	// console-logging the number inputted from the first page
+	let inputsContainer = $(".js__player-details-container");
 	let buttonPlayer = $(".js__button-players");
-	let container = $(".container");
 
-	buttonPlayer.on("click", () => console.log(myStorage.getItem("players")));
+	let playerDetails = [];
+
+// ****************************
+// 	EVENTS
+// ****************************
+
+	buttonEntry.on("click", () => {
+		// log the number of players in variable
+		players = inputEntry.val();
+
+		// output a corresponding number of inputs
+		let i = 0;
+
+		while (i < players) {
+			let fieldContainer = $("<div />");
+			fieldContainer.addClass("inputs-container js__inputs-container-" + i);
+
+			let labelFor = $("<label />");
+			labelFor.attr("id", "player-forename").text("First Name");
+
+			let forename = $("<input />");
+			forename.attr("id", "player-forename").attr("type", "text").attr("placeholder", "First Name").addClass("input-text js__forename-input");
+
+			let labelSur = $("<label />");
+			labelSur.attr("id", "player-surname").text("Last Name");
+
+			let surname = $("<input />");
+			surname.attr("id", "player-surname").attr("type", "text").attr("placeholder", "Last Name").addClass("input-text js__surname-input");
+
+			inputsContainer.append(fieldContainer);
+			fieldContainer.append(labelFor, forename, labelSur, surname);
+
+			i ++;
+		}
+	});
 
 });
 
