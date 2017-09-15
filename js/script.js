aa392af796879d3f6aacb0015251a52b1314922c
Nicola Richardson <nicolarichardson@Nicolas-MacBook-Pro.local>
2017-09-15 12:05:05 +0100

    players randomise and output as pairs on button click

--- js/script.js
+++ js/script.js
@@ -17,7 +17,32 @@ $($ => {
 	let surnames = [];
 	let playerDetails = [];
 
-	let trackerPage = $(".js__tracker-page");
+	let trackerSection = $(".js__tracker-section");
+
+
+// ****************************
+// 	FUNCTIONS
+// ****************************
+
+	// the Fisher-Yates (aka Knuth) Shuffle
+	function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
 
 
 // ****************************
@@ -33,23 +58,18 @@ $($ => {
 
 		// output a corresponding number of inputs
 		while (i < players) {
-			let fieldContainer = $("<div />");
-			fieldContainer.addClass("inputs-container js__inputs-container").attr("data-id", i);
+			let fieldContainer = $("<div />").addClass("inputs-container js__inputs-container").attr("data-id", i);
 
-			let labelFor = $("<label />");
-			labelFor.attr("id", "player-forename").text("First Name");
+			let labelFore = $("<label />").attr("id", "player-forename").text("First Name");
 
-			let forename = $("<input />");
-			forename.attr("id", "player-forename").attr("type", "text").attr("placeholder", "First Name").addClass("input-text js__forename-input");
+			let forename = $("<input />").attr("id", "player-forename").attr("type", "text").attr("placeholder", "First Name").addClass("input-text js__forename-input");
 
-			let labelSur = $("<label />");
-			labelSur.attr("id", "player-surname").text("Last Name");
+			let labelSur = $("<label />").attr("id", "player-surname").text("Last Name");
 
-			let surname = $("<input />");
-			surname.attr("id", "player-surname").attr("type", "text").attr("placeholder", "Last Name").addClass("input-text js__surname-input");
+			let surname = $("<input />").attr("id", "player-surname").attr("type", "text").attr("placeholder", "Last Name").addClass("input-text js__surname-input");
 
 			inputsContainer.append(fieldContainer);
-			fieldContainer.append(labelFor, forename, labelSur, surname);
+			fieldContainer.append(labelFore, forename, labelSur, surname);
 
 			i++;
 		}
@@ -70,37 +90,41 @@ $($ => {
 		});
 
 		// storing the details as objects in an array
-		for (let i = 0; i < forenames.length; i++){
+		for (let i = 0; i < players; i++){
 			let player = {
 				id: i,
 				forename: forenames[i],
 				surname: surnames[i],
+				// concatenating the names for display purposes
+				dispName: forenames[i].charAt(0).toUpperCase() + forenames[i].slice(1) + " " + surnames[i].charAt(0).toUpperCase(),
+				playing: true,
 			};
 
 			playerDetails.push(player);
-			// console.log(playerDetails);
 
 		};
 
+		console.log(playerDetails);
+
 // ****************************
-// 	TRACKER SECTION  - - - - - - but currently operating on previous section click
+// 	TRACKER SECTION  - - - - - - operating on previous section btn click for initial state
 // ****************************
 
-		// concatenating the names for display purposes
-		let fullName = playerDetails.map(({ forename, surname}) => forename + " " + surname.charAt(0));
+		// randomising the array
+		let playersRand = shuffle(playerDetails);
 
-		let playerList = $("<ul />");
-		trackerPage.append(playerList);
-
-		// outputting an individual <li> for each player name
-		fullName.map(player => {
-			let listItem = $("<li />").text(player);
-			playerList.append(listItem);
-		})
-
-		// console.log(fullName);
+		// output as pairs
+		for (let i = 0; i < playersRand.length; i += 2) {
+	    let playerList = $("<ul />").addClass("game-pairing js__round-1");
+	    let playerItem1 = $("<li />").text(playersRand[i].dispName);
+			let vs = $("<p />").text("vs");
+			let playerItem2 = $("<li />").text(playersRand[i+1].dispName);
+			
+			trackerSection.append(playerList); 
+			playerList.append(playerItem1, vs, playerItem2);
+		};
 
 	});
 
-});
+}); // document ready fn
 
