55f244e5e28bccf135a65770526863d8c455e945
Nicola Richardson <nicolarichardson@Nicolas-MacBook-Pro.local>
2017-09-16 19:52:41 +0100

    second re-factor to address second button click event

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $($ => {
 	// debugging mode activate / deactivated
 	const debugMode = true;
 	let debug = (message) => debugMode ? console.log(message) : null;
+	// ## DEBUG ## // 
 
 	// initialising the state
 	let players,
@@ -24,82 +25,28 @@ $($ => {
 		debug("state initialised");
 	};
 
-// ****************************
+	let reset = () => {
+		forenames = [];
+		surnames = [];
+		playerDetails = [];
+		valid = false;
 
-	init();
+		debug("values reset");
+	};
 
-// ****************************
-// 	VARIABLES
 // ****************************
 
 	const container = $(".js__wrapper");
 	const entrySection = $(".js__entry-section");
+	const detailsSection = $(".js__details-section");
 	const detailsContainer = $(".js__player-details-container");
 
-	let buttonEntry = $(".js__button-entry-numbers");
 	let errorEntry = $(".js__error-number-input")
-
-	let buttonPlayer = $(".js__button-players");
 	let errorDetail = $(".js__error-text-input");
 
-	let trackerSection = $(".js__tracker-section");
+// ******************************************************************************************************
 
-	// try to reduce the amount of global variables as there could be conflicts with browsers loading in other things and mistaking the $ signs for something else.
-
-	// consistency throughout - check how functions are declared, fat arrow or not? Keep it consistent throughout.
-
-// ****************************
-// 	FUNCTIONS
-// ****************************
-	
-	let storePlayerNames = () => {
-
-		let findInputsContainer = () => detailsContainer.find(".js__inputs-container");
-		let pushValToArray = (array, input) => array.push(input.val());
-
-		findInputsContainer().find("input").each((i, input) => {
-
-			if ($(input).hasClass("js__forename-input")) {
-				pushValToArray(forenames, $(input));
-
-				debug("forename " + i + " added to array");
-
-			}	else if ($(input).hasClass("js__surname-input")) {
-				pushValToArray(surnames, $(input));
-
-				debug("surname " + i + " added to array");
-
-			} else {
-				debug("no classes found on inputs");
-			}
-		});
-	};
-
-	let pushPlayersToArray = (numOfPlayers) => {
-		
-		let forenameFormatting = (forename) => forename.charAt(0).toUpperCase() + forename.slice(1);
-		let surnameFormatting = (surname) => surname.charAt(0).toUpperCase();
-
-		for (let i = 0; i < numOfPlayers; i++) {
-			let player = {
-				id: i,
-				forename: forenames[i],
-				surname: surnames[i],
-				fullName: forenames[i] + " " + surnames[i],
-				dispName: forenameFormatting(forenames[i]) + " " + surnameFormatting(surnames[i]),
-			};
-
-			playerDetails.push(player);
-		};
-
-		debug("pushPlayersToArray = " + playerDetails);
-
-		return playerDetails;
-	};
-
-// ****************************
-// 	NUMBER OF PLAYERS SECTION
-// ****************************
+	init();
 
 	// is there a maximum number of players?
 	let numberInputValidChecks = (value) => {
@@ -122,10 +69,10 @@ $($ => {
 				debug("number validation: input value is an odd number");
 
 			} else if (isEven(value)) {
+				
+				valid = true;
 
 				debug("number validation: SUCCESS - value is even");
-
-				return valid = true;
 
 			} else {
 				debug("number validation: conditional has broken at even/odd stage");
@@ -161,58 +108,193 @@ $($ => {
 
 	// is event delegation more efficient here rather than specifying the button itself?
 	entrySection.on("click", "button", () => {
-		let value = container.find($(".js__entry-numbers")).val();
+		let inputVal = container.find($(".js__entry-numbers")).val();
 		
-		numberInputValidChecks(value);
-		valid ? generateInputs(value) : debug("number validation failed");
+		numberInputValidChecks(inputVal);
+
+		valid ? generateInputs(inputVal) : debug("number validation failed");
+
+		debug("valid status on entry button click: " + valid);
+
+		valid = false;
+
+		debug("valid reset: " + valid);
 
 	});
 
-// ****************************
-// 	PLAYER DETAILS SECTION
-// ****************************
+// **************************************************************************************
 
-	buttonPlayer.on("click", () => {
+	let findInputs = () => detailsContainer.find(".js__inputs-container input");
+		
+	let toggleInputStyling = (textInput) => {
+		textInput.each((i, input) => {
+			$(input).val() === "" ? $(input).addClass("empty-field") : $(input).removeClass("empty-field");
+		
+			debug("input styling toggled");
+		});
+	};
 
-		let inputs = detailsContainer.find(".js__inputs-container input");
+	let textInputValidChecks = () => {
 
-		// finding the values entered into each input
-		let empty = inputs.filter(function() {
-      return this.value === "";
-    });
+		let emptyInputs = [];
 
-		// checking to see if the inputs have been filled
-    if(empty.length) {
-    	// if at least one input is empty
-    	debug("input empty");
+		let findEmptyInputs = () => {
+			findInputs().each((i, input) => {
+				$(input).val() === "" ? emptyInputs.push(i) : null;
 
-    	inputs.each(function() {
-    		$(this).val() === "" ? $(this).addClass("empty-field") : $(this).removeClass("empty-field");
-    	});
+			});
+		};
 
+		findEmptyInputs();
+
+		if (emptyInputs.length) {
+			
+			toggleInputStyling(findInputs());
 			errorDetail.text("First name and surname are both required fields, please enter all player details.");
 
-    } else {
-    	// no inputs are empty
+			debug("text validation: at least one field is empty");
+			debug("valid status when fields are empty: " + valid);
+
+		} else if (!emptyInputs.length) {
+				
+    	toggleInputStyling(findInputs());
+
+    	valid = true;
+
     	debug("inputs all filled");
+			debug("valid status when fields are completed: " + valid);
 
-    	inputs.removeClass("empty-field");
+		} else {
+				debug("text validation: conditional has broken at finding empty fields stage");
+
+		};
+	};
+
+	let populateTempPlayerArr = () => {
+		let tempPlayerArray = [];
+
+		storePlayerNames();
+		pushPlayersToArray(players, tempPlayerArray);
+
+		return tempPlayerArray;
+	};
+
+	let duplicates = [];
+
+	let findDuplicates = () => {
+		let fullNames = tempPlayerArray.map(player => player.fullName);
+
+		let fullNameIndex = fullNames.map((name, index) => {
+			return {
+				index: index,
+				indexOf: fullNames.indexOf(name),
+			}
+		});
+
+		duplicates = fullNameIndex.filter(item => item.index !== item.indexOf);
+		
+		debug("duplicates");
+		debug(duplicates);
+	};
+
+	let duplicateValidation = () => {
+
+		populateTempPlayerArr();
+		findDuplicates();
+
+		// if there are no empty input fields
+		if (valid) {
+			if (duplicates.length) {
+
+				// can I map over the duplicates array, and then map over the details array and find the entry/ies with matching index values, push those into a new array, then each over the inputs and if the forename and surname match, highlight the box??
+
+				valid = false;
+
+				errorDetail.text("There are duplicate names in your player list. Please make each player uniquely identifiable.");
+
+				debug("duplicate values found");
+
+			} else if (!duplicates.length) {
+
+				errorDetail.text("");
+				valid = true;
+
+				debug("no duplicate values found");
+				debug("valid status at duplicate validation: " + valid);
+
+			} else {
+
+				debug("duplicates conditional broken");
+			};
+		} else {
+		
+			debug("text input validation not passed, duplicate validation not run");
+		};
+	};
+
+	let printRandomPairs = () => {
+
+		let playersRand = shuffle(playerDetails);
+
+		// output as pairs
+		for (let i = 0; i < playersRand.length; i += 2) {
+	    let playerList = $("<ul />").addClass("game-pairing js__round-1");
+	    let playerItem1 = $("<li />").text(playersRand[i].dispName);
+			let vs = $("<p />").text("vs");
+			let playerItem2 = $("<li />").text(playersRand[i+1].dispName);
 			
-			// init();
-			storePlayerNames();
-			pushPlayersToArray(players);
+			container.find($(".js__tracker-section")).append(playerList); 
+			playerList.append(playerItem1, vs, playerItem2);
+		};
+	};
 
-			debug(playerDetails);
+	let storePlayerNames = () => {
 
-	// ****************************
-	// 	TRACKER SECTION  - - - - - - operating on previous section btn click for initial state
-	// ****************************
+		let pushValToArray = (array, input) => array.push(input.val());
 
-			// playerDetails.map(({ fullName }, i) => debug(i + " " + fullName));
+		findInputs().each((i, input) => {
 
-				// the Fisher-Yates (aka Knuth) Shuffle
-	function shuffle(array) {
-	  var currentIndex = array.length, temporaryValue, randomIndex;
+			if ($(input).hasClass("js__forename-input")) {
+				pushValToArray(forenames, $(input));
+
+				debug("forename " + i + " added to array");
+
+			}	else if ($(input).hasClass("js__surname-input")) {
+				pushValToArray(surnames, $(input));
+
+				debug("surname " + i + " added to array");
+
+			} else {
+				debug("no classes found on inputs");
+			}
+		});
+	};
+
+	let pushPlayersToArray = (numOfPlayers, array) => {
+		
+		let forenameFormatting = (forename) => forename.charAt(0).toUpperCase() + forename.slice(1);
+		let surnameFormatting = (surname) => surname.charAt(0).toUpperCase();
+
+		for (let i = 0; i < numOfPlayers; i++) {
+			let player = {
+				id: i,
+				forename: forenames[i],
+				surname: surnames[i],
+				fullName: forenames[i] + " " + surnames[i],
+				dispName: forenameFormatting(forenames[i]) + " " + surnameFormatting(surnames[i]),
+			};
+
+			array.push(player);
+		};
+
+		debug(array);
+
+		return array;
+	};
+
+	// the Fisher-Yates (aka Knuth) Shuffle
+	let shuffle = (array) => {
+	  let currentIndex = array.length, temporaryValue, randomIndex;
 
 	  // While there remain elements to shuffle...
 	  while (0 !== currentIndex) {
@@ -230,68 +312,33 @@ $($ => {
 	  return array;
 	};
 
-			let fullNames = playerDetails.map(player => player.fullName);
-			let duplicate = fullNames.some((name, i) => fullNames.indexOf(name) !== i);
+	detailsSection.on("click", "button", () => {
+		reset();
+		textInputValidChecks();
 
-// 			var fullNames = playerDetails.map(function(player) {
-// 				return player.fullName;
-// 			});
-// [
-// person a,
-// person b,
-// person a,
-// person x
-// ]
-// 			var fullNameIndex = fullNames.map(function(name, index) {
-// 				return {
-// 					index: index,
-// 					indexOf: fullNames.indexOf(name)
-// 				};
-// 			});
-// [
-// {index: 0, indexOf: 0},
-// {index: 1, indexOf: 1},
-// {index: 2, indexOf: 0}, *
-// {index: 3, indexOf: 3},
-// ]
-// 			var duplicates = fullNameIndex.filter(function(item) {
-// 				return item.index !== item.indexOf;
-// 			});
-// [
-// {index: 2, indexOf: 0},
-// ]
+		if (valid) {
+			duplicateValidation();
 
-			// get array of full names of players (map)
-			// get indexOf from full names of players (map)
+			if (valid) {
+				storePlayerNames();
+				pushPlayersToArray(players, playerDetails);
+				printRandomPairs();
 
-			if (duplicate === true) {
-				// duplicate full names found
-				debug("duplicates found");
-
-				errorDetail.text("There are duplicate names in your player list. Please make each player uniquely identifiable.");
-
-				addPlayers();
+				debug("valid status on player button click: " + valid);
+				valid = false;
+				debug("valid reset: " + valid);
 
 			} else {
-				// no duplicates have been found
-				errorDetail.text("");
+				debug("text duplicate validation failed");
+				debug("valid status at failed duplicate validation: " + valid);
+			}
 
-				// randomising the array
-				let playersRand = shuffle(playerDetails);
+		} else {
+			debug("text input validation failed");
+			debug("valid status at failed input validation: " + valid);
+		}
 
-				// output as pairs
-				for (let i = 0; i < playersRand.length; i += 2) {
-			    let playerList = $("<ul />").addClass("game-pairing js__round-1");
-			    let playerItem1 = $("<li />").text(playersRand[i].dispName);
-					let vs = $("<p />").text("vs");
-					let playerItem2 = $("<li />").text(playersRand[i+1].dispName);
-					
-					trackerSection.append(playerList); 
-					playerList.append(playerItem1, vs, playerItem2);
-
-				};
-			};
-		};
 	});
+
 }); // document ready fn
 
